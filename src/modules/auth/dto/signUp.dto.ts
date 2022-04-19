9127805eca4754b0af694cbb815c45e184fd9be2
Matej Posavac <matejposavac27@gmail.com>
2022-04-19 11:24:46 +0200

    Added swagger

--- src/modules/auth/dto/signUp.dto.ts
+++ src/modules/auth/dto/signUp.dto.ts
@@ -1,19 +1,23 @@
 import { IsDefined, IsEmail, IsString, IsUUID } from 'class-validator';
-//import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class SignUpDto {
+  @ApiProperty()
   @IsDefined()
   @IsUUID('4')
   id: string;
 
+  @ApiProperty()
   @IsDefined()
   @IsEmail()
   email: string;
 
+  @ApiPropertyOptional()
   @IsDefined()
   @IsString()
   password?: string;
 
+  @ApiPropertyOptional()
   @IsDefined()
   @IsString()
   access_token?: string;
--- src/modules/staff/staff.controller.ts
+++ src/modules/staff/staff.controller.ts
@@ -1,16 +1,27 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { StaffService } from './staff.service';
 
+@ApiTags('Staff')
 @Controller('staff')
 export class StaffController {
   constructor(private readonly staffService: StaffService) {}
 
   @Get('availability/:mat_broj')
+  @ApiOkResponse({
+    description: 'OK.',
+    isArray: true,
+    //TODO: add type: GetStaffAvailabilty
+  })
   getAvailabilty(@Param('mat_broj') mat_broj) {
     return this.staffService.getAvailability(mat_broj);
   }
 
   @Get('list')
+  @ApiOkResponse({
+    description: 'OK.',
+    //TODO: add type: GetStaffList
+  })
   getStaffList() {
     return this.staffService.getStaffList();
   }
